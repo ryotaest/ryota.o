6fcd287a4d5e409c3d369712a83cc923fa8be816
ryotaest <118783403+ryotaest@users.noreply.github.com>
2024-05-24 22:16:03 +1000

    re commit

---
+++ Profile/script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector(".menu-icon");
+  const menuItems = document.querySelector(".menu-items");
+
+  menuIcon.addEventListener("click", function () {
+    menuItems.classList.toggle("active");
+  });
+});
+
+function openGoogleForm() {
+  document.getElementById("googleFormOverlay").style.display = "block";
+}
+
+function closeGoogleForm() {
+  document.getElementById("googleFormOverlay").style.display = "none";
+}
+
+function toggleText() {
+  const fullText = document.getElementById("intro-full");
+  const button = document.getElementById("toggle-btn");
+
+  if (fullText.style.display === "none") {
+    fullText.style.display = "block";
+    button.textContent = "Less";
+  } else {
+    fullText.style.display = "none";
+    button.textContent = "More";
+  }
+}
+
+function fadeAnime() {
+  $(".fadeUpTrigger").each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("fadeUp");
+    } else {
+      $(this).removeClass("fadeUp");
+    }
+  });
+}
+
+$(window).scroll(function () {
+  fadeAnime();
+});
